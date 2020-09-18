fbbf39263820a7cc8e0d3b98df0cb40cbc5945b0
caiweida <2332268008@qq.com>
2020-09-18 17:32:02 +0800

    11111

--- main.js
+++ main.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import uView from 'uview-ui'
 import oSit from 'common/overallSituation.js' //引入全局js文件
+import Ctky from 'common/Ctky.js' //传统客运
 
 Vue.use(uView)
 Vue.config.productionTip = false
@@ -11,4 +12,15 @@ App.mpType = 'app'
 const app = new Vue({
     ...App
 })
+
+//模拟异步请求数据 - 传统客运
+const ctky = type=>{
+	return new Promise(resolve=>{
+		setTimeout(()=>{
+			resolve(Ctky[type]);
+		}, 500)
+	})
+}
+
+Vue.prototype.$api = {ctky};
 app.$mount()
