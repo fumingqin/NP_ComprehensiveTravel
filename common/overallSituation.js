ad1da67ddeaa3231bfd15af67a135569435e6683
Gongjs <1845553629@qq.com>
2020-09-24 17:28:09 +0800

    1

--- common/overallSituation.js
+++ common/overallSituation.js
@@ -21,6 +21,7 @@ const Interface = {
 		applyName:'南平综合出行',
 		
 		// appName:'专线购票小程序', //仅用于微信授权登录
+		//appName:'交通在线', //仅用于微信授权登录
 		appName:'南平综合出行', //仅用于微信授权登录
 		imageApplicationType:'XCX',//全局图片接口应用类型，用于请求全局图片使用
 		// #ifdef H5
@@ -41,6 +42,7 @@ const appConfig={  //AppId,AppSecret配置
 		// wxAppId:'wxb9df0ad8d98f94a1', //专线购票
 		//wxAppId:'wx37b8f978e30050e4', //南平综合出行
 		wxAppId:'wxf64bf945b37a9939', //南平综合出行09-21
+		//wxAppId:'wxdf94dc8c9eb42f63', //交通在线小程序09-24
 	},
 	H5Config:{
 		name:'公众号的Appid和App秘钥',
