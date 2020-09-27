71ba29f2bbc010fa0e19fe5a76a25e8bea5a5ba7
Gongjs <1845553629@qq.com>
2020-09-27 15:45:01 +0800

    修改广告

--- common/changeImage.js
+++ common/changeImage.js
@@ -8,43 +8,25 @@ import $oSit from '@/common/overallSituation.js';
 
 
 //获取图片的路径
-async function GetImage(systemName,type) {
-	// #ifdef MP-WEIXIN
-	// #endif
-	var openidtype="XCX";
-	var model=-1;
-	if(systemName=="南平综合出行"){
-		model=5;
-	}else if(systemName=="漳州达达通"){
-		model=11;
-	}
-	
-	if(model==-1){
-		console.log('项目名暂未添加图片')
-	}else{
-		var res = await request(model,systemName,openidtype,type)
-		return res;
-	}
+async function GetImage(type,title) {
+	var res = await request(type,title);
+	return res;
 }
 
 //request请求
-function request (model,systemName,openidtype,type) {
+function request (type,title) {
   return new Promise(function (resolve, reject) {
-	  // console.log("系统名称",systemName)
-	  // console.log("应用类型","XCX")
-	  // console.log("访问地址",Url+'/api/zhcx/getImage')
     uni.request({
-    	url:Url+'/api/zhcx/getImage',
-    	data:{
-    		model:model, //模块名称
-			systemtype:openidtype,//应用类型
-			companyid:systemName, //公司名称
-			type:type, //图片类型
-    	},
+    	url:Url+'/api/BasicImage/GetRotationChart',
     	method:'POST',
     	success(res){
-    		// console.log("获取图片",res)
-			resolve(res.data.data.imageUrl)
+			console.log(res,"图片");
+			var data = res.data.data;
+			for(var item of data){
+				if(item.Title == title && item.Type == type){
+					resolve(item.ImageURL);
+				}
+			}
     	},
     	fail(err){
     		console.log(err,"err")
